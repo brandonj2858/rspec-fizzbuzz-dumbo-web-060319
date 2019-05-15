16e14de06824f61863dc22386f358e7ebccb30dc
Brandon James <brandonj2858@gmail.com>
2019-05-15 17:52:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if fizzbuzz(int) % 3
+  if fizzbuzz() % 3
     return "Fizz"
-  else if fizzbuzz(int) % 5
+  else if fizzbuzz() % 5
     return "Buzz"
-  else if fizzbuzz(int) % 5 && 3
+  else if fizzbuzz() % 5 && 3
     return "Fizzbuzz"
   else return false end
 end
